31ae29609e65e3ee5481e7595958b5b3bd1f5c65
Nicolas Souza <nszchagas@gmail.com>
2023-06-26 11:14:17 -0300

    Configurando deploy

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,33 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.1.1"
-	id("io.spring.dependency-management") version "1.1.0"
+    java
+    id("org.springframework.boot") version "3.1.1"
+    id("io.spring.dependency-management") version "1.1.0"
 }
 
 group = "br.com.rihappy"
 version = "0.0.1-SNAPSHOT"
 
+
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
+
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	runtimeOnly("org.postgresql:postgresql")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    runtimeOnly("org.postgresql:postgresql")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Jar>{ 
+    enabled = false
+
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
